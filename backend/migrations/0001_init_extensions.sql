144c526ac7f17075a6bc215e89ea313c1fe0be1e
ZangDev <ff14eternitalis@gmail.com>
2026-04-18 16:55:54 +0200

    fix: make pgvector optional in startup migrations

--- backend/migrations/0001_init_extensions.sql
+++ backend/migrations/0001_init_extensions.sql
@@ -1,4 +1,8 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-CREATE EXTENSION IF NOT EXISTS "vector";
+DO $$
+BEGIN
+    IF EXISTS (SELECT 1 FROM pg_available_extensions WHERE name = 'vector') THEN
+        CREATE EXTENSION IF NOT EXISTS "vector";
+    END IF;
+END $$;
 CREATE EXTENSION IF NOT EXISTS "pg_trgm";
-
