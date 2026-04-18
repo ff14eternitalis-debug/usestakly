98447a7c68e8a27b267f65c21837c5170429c82f
ZangDev <ff14eternitalis@gmail.com>
2026-04-18 16:19:52 +0200

    fix: lazily initialize postgres pool on startup

--- backend/src/db/mod.rs
+++ backend/src/db/mod.rs
@@ -5,6 +5,5 @@ pub async fn connect(database_url: &str) -> Result<PgPool, sqlx::Error> {
     PgPoolOptions::new()
         .max_connections(5)
         .acquire_timeout(Duration::from_secs(5))
-        .connect(database_url)
-        .await
+        .connect_lazy(database_url)
 }
